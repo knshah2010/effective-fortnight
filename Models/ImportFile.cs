fc28fe3ea766f2c85eccc4169ae6a512bdf79992
Meet <meetkanzariya@gmail.com>
2020-11-05 17:14:25 +0530

    import model

--- Models/ImportFile.cs
+++ Models/ImportFile.cs
@@ -1,10 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Framework.Models;
+using Dapper.Contrib.Extensions;
+using FluentValidation;
+using Framework.Library.Validator;
+using System;
+using Framework.Library.Helper;
+using System.Numerics;
 
 namespace Models
 {
-    class ImportFile
+    [Table("import_file")]
+    public class ImportFile :BaseModel
     {
+        [Key]
+        [ExplicitKey]
+        public int code { get; set; }
+        public string operation_type { get; set; }
+        public string process_name { get; set; }
+        public string file_name { get; set; }
+        public string file_path { get; set; }
+        public string error_file_path { get; set; }
+        public int total_count { get; set; }
+        /* 
+         * 1=upload
+         * 2=in process
+         * 3=complete
+         */
+        public char file_status { get; set; } = '1';
+        /*
+         * 0 : not processed {bydefault this set} {while upload file}
+         * 1 : successfully processed all record
+         * 2 : there are error in some record
+         */
+        public char record_status { get; set; } = '0';
+        public int success_count { get; set; }
+        public int error_count { get; set; }
+        public string error_data { get; set; }
+
+        [Computed]
+        public new string flg_sentbox_entry { get; set; }
+        //public string created_by { get; set; }
+        //public DateTime created_at { get; set; }
+    }
+    public class ImportFileValidator : AbstractValidator<ImportFile>
+    {
+        public ImportFileValidator()
+        {
+            RuleFor(d => d.process_name).Require();
+        }
     }
 }
