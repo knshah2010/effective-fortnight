dfc171d9f2f70080b9f789e6e43a418bb4e1ea96
Meet <meetkanzariya@gmail.com>
2020-11-11 17:40:46 +0530

    master list service

--- MasterDataExchange/Areas/Service/Controllers/MasterController.cs
+++ MasterDataExchange/Areas/Service/Controllers/MasterController.cs
@@ -1,9 +1,13 @@
 ﻿using DataExchange.Areas.Service.BAL;
 using Framework.Controllers;
+using Framework.CustomDataType;
 using Framework.Extension;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Models;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Dynamic;
 
 namespace DataExchange.Areas.Service.Controllers
 {
@@ -18,12 +22,45 @@ namespace DataExchange.Areas.Service.Controllers
             MasterBal _bal = new MasterBal();
             return _bal.SaveBmc(data.ParseRequestList<Bmc>());
         }
+
+        [HttpGet]
+        [Route("bmclist")]
+        public IActionResult BmcList()
+        {
+            SearchParam Param = new SearchParam()
+            {
+            };
+            dynamic data = new ExpandoObject();
+            data.data = Param;
+            ListEngine _listEngine = new ListEngine();
+            SetParam();
+            return _listEngine.List(JObject.FromObject(data), FileName, "bmc");
+        }
+
         [HttpPost]
         [Route("route")]
         public IActionResult Route([FromBody]object data)
         {
             MasterBal _bal = new MasterBal();
             return _bal.SaveRoute(data.ParseRequestList<Route>());
+        }
+
+        [HttpGet]
+        [Route("routelist")]
+        public IActionResult RouteList(string bmc_code)
+        {
+            SearchParam Param = new SearchParam()
+            {
+                Param = new List<ConditionParameter>()
+                {
+                     new ConditionParameter{PropertyName="bmc_code",PropertyValue=bmc_code},
+                }
+            };
+            dynamic data = new ExpandoObject();
+            data.data = Param;
+            ListEngine _listEngine = new ListEngine();
+            SetParam();
+            return _listEngine.List(JObject.FromObject(data), FileName, "route");
         }
 
         [HttpPost]
@@ -34,6 +71,24 @@ namespace DataExchange.Areas.Service.Controllers
             return _bal.SaveMpp(data.ParseRequestList<Dcs>());
         }
 
+        [HttpGet]
+        [Route("dcslist")]
+        public IActionResult DcsList(string bmc_code)
+        {
+            SearchParam Param = new SearchParam()
+            {
+                Param = new List<ConditionParameter>()
+                {
+                     new ConditionParameter{PropertyName="d.bmc_code",PropertyValue=bmc_code},
+                }
+            };
+            dynamic data = new ExpandoObject();
+            data.data = Param;
+            ListEngine _listEngine = new ListEngine();
+            SetParam();
+            return _listEngine.List(JObject.FromObject(data), FileName, "dcs");
+        }
+
         [HttpPost]
         [Route("member")]
         public IActionResult Member([FromBody] object data)
@@ -42,12 +97,48 @@ namespace DataExchange.Areas.Service.Controllers
             return _bal.SaveMember(data.ParseRequestList<Member>());
         }
 
+        [HttpGet]
+        [Route("memberlist")]
+        public IActionResult MemberList(string dcs_code)
+        {
+            SearchParam Param = new SearchParam()
+            {
+                Param = new List<ConditionParameter>()
+                {
+                     new ConditionParameter{PropertyName="m.dcs_code",PropertyValue=dcs_code},
+                }
+            };
+            dynamic data = new ExpandoObject();
+            data.data = Param;
+            ListEngine _listEngine = new ListEngine();
+            SetParam();
+            return _listEngine.List(JObject.FromObject(data), FileName, "member");
+        }
+
         [HttpPost]
         [Route("customer")]
         public IActionResult Customer([FromBody] object data)
         {
             MasterBal _bal = new MasterBal();
             return _bal.SaveCustomer(data.ParseRequestList<CustomerMaster>());
+        }
+
+        [HttpGet]
+        [Route("customerlist")]
+        public IActionResult CustomerList(string bmc_code)
+        {
+            SearchParam Param = new SearchParam()
+            {
+                Param = new List<ConditionParameter>()
+                {
+                     new ConditionParameter{PropertyName="c.bmc_code",PropertyValue=bmc_code},
+                }
+            };
+            dynamic data = new ExpandoObject();
+            data.data = Param;
+            ListEngine _listEngine = new ListEngine();
+            SetParam();
+            return _listEngine.List(JObject.FromObject(data), FileName, "customer");
         }
 
         [HttpPost]
