13250360caf76d3d8fb9316168a48e9167b6cc0a
Meet <meetkanzariya@gmail.com>
2020-11-20 09:13:03 +0530

    Inbox and Sentbox models

--- AndroidAmcu/Areas/Configuration/Models/Inbox.cs
+++ AndroidAmcu/Areas/Configuration/Models/Inbox.cs
@@ -1,11 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Framework.Models;
+using Dapper.Contrib.Extensions;
+using System;
 
 namespace AndroidAmcu.Areas.Configuration.Models
 {
-    public class Inbox
+    [Table("tbl_inbox")]
+    public class Inbox : BaseModel
     {
+        [ExplicitKey]
+        public string uuid { get; set; }
+        public string sync_status { get; set; }
+        public string source_org_type { get; set; }
+        public string source_org_id { get; set; }
+        public string dest_org_type { get; set; }
+        public string dest_org_id { get; set; }
+        public string message_type { get; set; }
+        public string table_name { get; set; }
+        public string operation { get; set; }
+        public string json_text { get; set; }
+        public string error_log { get; set; }
+        public int sequence_no { get; set; }
+        public string originating_org_id { get; set; }
+        public string originating_org_type { get; set; }
+        public DateTime posting_timestamp { get; set; }
+        public DateTime sync_timestamp { get; set; }
+        public string source_device_mac { get; set; }
+        public string version_no { get; set; }
+        public string device_id { get; set; }
+        public DateTime error_timestamp { get; set; }
+        [Computed]
+        public new string flg_sentbox_entry { get; set; } = "N";
+        [Computed]
+        public new int originating_type { get; set; } = 0;
+        [Computed]
+        public new string originating_org_code { get; set; }
     }
 }
