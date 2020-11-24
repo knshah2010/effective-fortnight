453eaf4ce212416de3161ce28870622dea1718b9
Meet <meetkanzariya@gmail.com>
2020-11-24 16:57:56 +0530

    BmcMilkType model

---
+++ Models/BmcMilkType.cs
@@ -0,0 +1,31 @@
+﻿using Framework.Models;
+using Dapper.Contrib.Extensions;
+using FluentValidation;
+using Framework.Library.Validator;
+using System;
+using Framework.Library.Helper;
+
+namespace Models
+{
+    [Table("tbl_bmc_milk_type")]
+    public class BmcMilkType : BaseModel
+    {
+        public string bmc_code { get; set; }
+        public int milk_type_code { get; set; }
+        public bool is_active { get; set; }
+        [Computed]
+        public new string flg_sentbox_entry { get; set; } = "N";
+        public string originating_org_type { get; set; } = "portal";
+    }
+
+    public class BmcMilkTypeValidator : AbstractValidator<BmcMilkType>
+    {
+        public BmcMilkTypeValidator()
+        {
+            RuleFor(d => d.bmc_code).Require();
+            RuleFor(d => d.milk_type_code).Require();
+            RuleFor(d => d.is_active).Require();
+        }
+
+    }
+}
